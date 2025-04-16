89bd50c42db278bec3ddb9ac821d1efe53b49dfc
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2025-04-16 18:29:11 +0300

    Update BillApiException.php

--- src/Exception/BillApiException.php
+++ src/Exception/BillApiException.php
@@ -4,7 +4,7 @@ namespace Haikiri\MikBiLL\Exception;
 
 use Exception;
 
-/** Отбрасываем исключение все остальные исключения MikBiLL. */
+/** Отбрасываем все остальные исключения MikBiLL. */
 class BillApiException extends Exception
 {
 	public function __construct(string $message = "Unknown error", int $code = 0)

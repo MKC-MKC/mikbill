1fcf41cdf7cf60b2dc4828429a13dc66450c2506
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2025-05-24 18:02:25 +0300

    фикс типизации данных

--- src/Cabinet/Services/Credit.php
+++ src/Cabinet/Services/Credit.php
@@ -37,110 +37,110 @@ class Credit extends ResponseWrapper
 
 	/**
 	 * Метод возвращает стоимость активации кредита.
-	 * @return float
+	 * @return float|null
 	 */
-	public function getActivateCost(): float
+	public function getActivateCost(): float|null
 	{
-		return $this->getData("info.active_cena", 0);
+		return $this->getData("info.active_cena");
 	}
 
 	/**
 	 * Метод возвращает процент кредита.
-	 * @return int
+	 * @return int|null
 	 */
-	public function getCreditPercent(): int
+	public function getCreditPercent(): int|null
 	{
-		return $this->getData("info.credit_procent", 0);
+		return $this->getData("info.credit_procent");
 	}
 
 	/**
 	 * Метод возвращает тип кредита.
-	 * @return int
+	 * @return int|null
 	 */
-	public function getType(): int
+	public function getType(): int|null
 	{
-		return $this->getData("info.type", 0);
+		return $this->getData("info.type");
 	}
 
 	/**
 	 * Метод возвращает тип кредита.
-	 * @return string
+	 * @return string|null
 	 */
-	public function getCurrency(): string
+	public function getCurrency(): string|null
 	{
-		return $this->getData("info.currency", "N");
+		return $this->getData("info.currency");
 	}
 
 	/**
 	 * Метод возвращает сумму кредита.
-	 * @return float
+	 * @return float|null
 	 */
-	public function getCreditSum(): float
+	public function getCreditSum(): float|null
 	{
-		return $this->getData("info.credit_summa", 0);
+		return $this->getData("info.credit_summa");
 	}
 
 	/**
 	 * Метод проверяет возможность досрочного погашения кредита.
-	 * @return bool
+	 * @return bool|null
 	 */
-	public function canEarlyLoanRepayment(): bool
+	public function canEarlyLoanRepayment(): bool|null
 	{
-		return $this->getData("info.early_loan_repayment", false);
+		return $this->getData("info.early_loan_repayment");
 	}
 
 	/**
 	 * Метод возвращает количество дней, на которое будут доступны услуги.
-	 * @return int
+	 * @return int|null
 	 */
-	public function getAvailableDays(): int
+	public function getAvailableDays(): int|null
 	{
-		return $this->getData("info.will_be_avilable_days", 0);
+		return $this->getData("info.will_be_avilable_days");
 	}
 
 	/**
 	 * Метод возвращает число 'G' активации кредита.
-	 * @return string
+	 * @return string|null
 	 */
-	public function getCreditDayStart(): string
+	public function getCreditDayStart(): string|null
 	{
-		return $this->getData("info.start_credit_day", "0");
+		return $this->getData("info.start_credit_day");
 	}
 
 	/**
 	 * Метод возвращает число 'G' отключения кредита.
-	 * @return string
+	 * @return string|null
 	 */
-	public function getCreditDayStop(): string
+	public function getCreditDayStop(): string|null
 	{
-		return $this->getData("info.stop_credit_day", "0");
+		return $this->getData("info.stop_credit_day");
 	}
 
 	/**
 	 * Метод возвращает объект даты активации кредита.
-	 * @return DateTime|bool
+	 * @return DateTime|bool|null
 	 */
-	public function getDateStart(): DateTime|bool
+	public function getDateStart(): DateTime|bool|null
 	{
 		return DateTime::createFromFormat("Y-m-d", $this->getData("info.date_start"));
 	}
 
 	/**
 	 * Метод возвращает объект даты отключения кредита.
-	 * @return DateTime|bool
+	 * @return DateTime|bool|null
 	 */
-	public function getDateStop(): DateTime|bool
+	public function getDateStop(): DateTime|bool|null
 	{
 		return DateTime::createFromFormat("Y-m-d", $this->getData("info.date_stop"));
 	}
 
 	/**
 	 * Метод возвращает количество дней на которое будет активирован кредит.
-	 * @return int
+	 * @return int|null
 	 */
-	public function getActivationDays(): int
+	public function getActivationDays(): int|null
 	{
-		return $this->getData("info.credit_active_days", 0);
+		return $this->getData("info.credit_active_days");
 	}
 
 }

8f949965bcdfa738a6c86dead71c0bf94b90ecbf
Haikiri <59623220+MKC-MKC@users.noreply.github.com>
2026-02-14 10:46:38 +0300

    Update MikBiLLApiMock.php

--- tests/Mock/MikBiLLApiMock.php
+++ tests/Mock/MikBiLLApiMock.php
@@ -10,8 +10,8 @@ class MikBiLLApiMock extends MikBiLLApi
 {
 	private static ?string $mockedData;
 	private string $receivedHmacKey;
-	private static string $expected_key = "mockedSignKey";
-	private static string $expected_token = "Bearer eyJ0eXAiOi.JKV1QiLCJ.hbGciOiJIUzI.1NiJ9";
+	private const EXPECTED_SALT = "mockedSignKey";
+	private const EXPECTED_TOKEN = "Bearer eyJ0eXAiOi.JKV1QiLCJ.hbGciOiJIUzI.1NiJ9";
 
 	public function __construct(string $url, string $key, mixed $mockedData = null)
 	{
@@ -37,15 +37,15 @@ class MikBiLLApiMock extends MikBiLLApi
 			}
 
 			# Имитируем проверку ключа на стороне Api.
-			$expectedSign = hash_hmac("sha512", $salt, self::$expected_key);
+			$expectedSign = hash_hmac("sha512", $salt, self::EXPECTED_SALT);
 			if (!hash_equals($expectedSign, $sign)) $response = self::onUnauthorized();
 		} else {
 			if (self::$debug) {
 				error_log("Received Token: " . $token);
-				error_log("Expected Token: " . self::$expected_token);
+				error_log("Expected Token: " . self::EXPECTED_TOKEN);
 			}
 			if ($token === "") throw new Exception\UnauthorizedException("The token was not found: The storage with token is empty.", -999);
-			if (!is_null($token) && $token !== self::$expected_token) $response = self::onUnauthorized();
+			if (!is_null($token) && $token !== self::EXPECTED_TOKEN) $response = self::onUnauthorized();
 		}
 
 		# Проверяем и возвращаем данные.
